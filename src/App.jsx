7d4887bf0ff2230439f3cfe427646b4ac21e7cd3
byaurea <aureacarminato@gmail.com>
2025-09-25 16:45:19 -0300

    add/react & tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-pink-300">
+        Olá, mundo com Tailwind!
+      </h1>
+    </>
+  );
+}
+
+export default App;
